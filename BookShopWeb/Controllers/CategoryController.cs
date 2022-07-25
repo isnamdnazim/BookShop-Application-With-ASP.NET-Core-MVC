370a39a49828c992b1174a501486182e678349d4
Md Nazim Uddin <isnamdnazim@gmail.com>
2022-07-26 03:46:12 +0600

    Working

--- BookShopWeb/Controllers/CategoryController.cs
+++ BookShopWeb/Controllers/CategoryController.cs
@@ -42,5 +42,43 @@ namespace BookShopWeb.Controllers
             return View(obj);   
             
         }
+
+        //Get
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(c=>c.Id==id);
+            //var categoryFromDbSingle = _db.Categories.SingleOrDefault(c=>c.Id==id);
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
     }
 }
--- BookShopWeb/Models/Category.cs
+++ BookShopWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookShopWeb.Models
 {
@@ -8,6 +9,8 @@ namespace BookShopWeb.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order Must be between 1 and 100 only!!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
